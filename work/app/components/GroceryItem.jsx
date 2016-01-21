a7ec0172a1d0fb9047fa75ca65b9200bf7743767
c.neculita <c.neculita@levi9.com>
2016-01-21 15:33:36 +0200

    Front end

---
+++ work/app/components/GroceryItem.jsx
@@ -0,0 +1,31 @@
+var React = require('react/addons');
+var action = require('./../actions/GroceryActionItemCreator.jsx')
+
+module.exports = React.createClass({
+    delete: function(e){
+        e.preventDefault();
+        action.delete(this.props.item);
+    },
+    togglePurchased: function(e){
+        e.preventDefault();
+        if(this.props.item.purchased)
+            action.unbuy(this.props.item);
+        else
+            action.buy(this.props.item);
+    },
+    render: function(){
+        return(
+             <div className="grocery-item row">
+                <div className="six columns">
+                    <h4 className={this.props.item.purchased ? "strikethrough" : ""}>{this.props.item.name}</h4>
+                </div>
+                <form className ="three columns" onSubmit={this.togglePurchased} className="three columns">
+                    <button className = {this.props.item.purchased ? "": "button-primary"}> {this.props.item.purchased ?"Unbuy":"Buy"} </button>
+                </form>
+                <form className ="three columns" onSubmit={this.delete}>
+                    <button> &times; </button>
+                </form>
+             </div>
+        )  
+    }
+})
